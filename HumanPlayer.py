559db37d847365fd59f000a3e0d12f17b5e4ac2d
sapire <sapir8886@gmail.com>
2021-02-24 13:22:19 +0200

    added general algorithm of 'place_submarines' in class HumanPlayer

--- HumanPlayer.py
+++ HumanPlayer.py
@@ -6,7 +6,14 @@ class HumanPlayer(IPlayer):
         IPlayer.__init__(self, name)
 
     def get_move(self):
+
         pass
 
     def place_submarines(self):
+        # display message 'Please place all your ships on the board. Ships cannot be on top of each other. All ships
+        # must be placed before the game can begin'
+
+        # show Carrier (5 tiles) to player, the ship should follow the mouse of the player's cursor
+        # when the user clicks, the submarine should be placed
+        # move on to place the next submarine until none are left
         pass
