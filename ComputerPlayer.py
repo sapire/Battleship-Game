f4cf9ec25d53b38758643ab3aa256240a3c2f767
sapire <sapir8886@gmail.com>
2021-03-01 18:26:02 +0200

    made changes to 'get_move' so we don't repeat the same move

--- ComputerPlayer.py
+++ ComputerPlayer.py
@@ -11,63 +11,96 @@ class ComputerPlayer(IPlayer):
         self.nearby_moves = []  # will hold the nearby tiles from the hit_list.
         # for example if in the hit_list we have (2,2), then in the nearby_moves
         # we will have [(1,2), (3, 2), (2, 1), (2, 3)]
+        self.moves_made = []  # will hold all the moves (attacks) that were attempted by the computer, whether they
+        # were a hit or a miss
 
     def add_to_hit_list(self, coordinate):
         self.hit_list.append((coordinate[0], coordinate[1]))
+
+    def random_attack(self):
+        rand_col = random.randint(0, 9)
+        rand_row = random.randint(0, 9)
+        is_hit = self.player_board.check_hit([rand_row, rand_col])  # make move and check if hit
+        if is_hit is True:  # check if hit, if so add to hit_list
+            self.hit_list.append((rand_row, rand_col))
+        return rand_row, rand_col
+
+    def find_nearby_moves(self):
+        x, y = self.hit_list.pop()
+        if x == 0 or y == 0 or x == 9 or y == 9:  # "walls" of the board
+            if x == 0 and y == 0:  # top left corner
+                if (x, y + 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y + 1))
+                if (x+1, y) not in self.moves_made:
+                    self.nearby_moves.append((x + 1, y))
+            elif x == 9 and y == 9:  # bottom right corner
+                if (x - 1, y) not in self.moves_made:
+                    self.nearby_moves.append((x - 1, y))
+                if (x, y - 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y - 1))
+            elif x == 0 and y == 9:  #
+                if (x + 1, y) not in self.moves_made:
+                    self.nearby_moves.append((x + 1, y))
+                if (x, y - 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y - 1))
+            elif x == 9 and y == 0:
+                if (x - 1, y) not in self.moves_made:
+                    self.nearby_moves.append((x - 1, y))
+                if (x, y + 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y + 1))
+            elif x == 0:
+                if (x + 1, y) not in self.moves_made:
+                    self.nearby_moves.append((x + 1, y))
+                if (x, y + 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y + 1))
+                if (x, y - 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y - 1))
+            elif x == 9:
+                if (x - 1, y) not in self.moves_made:
+                    self.nearby_moves.append((x - 1, y))
+                if (x, y - 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y - 1))
+                if (x, y + 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y + 1))
+            elif y == 0:
+                if (x - 1, y) not in self.moves_made:
+                    self.nearby_moves.append((x - 1, y))
+                if (x + 1, y) not in self.moves_made:
+                    self.nearby_moves.append((x + 1, y))
+                if (x, y + 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y + 1))
+            elif y == 9:
+                if (x + 1, y) not in self.moves_made:
+                    self.nearby_moves.append((x + 1, y))
+                if (x - 1, y) not in self.moves_made:
+                    self.nearby_moves.append((x - 1, y))
+                if (x, y - 1) not in self.moves_made:
+                    self.nearby_moves.append((x, y - 1))
+        else:  # not on the walls or corners so we can move anywhere
+            if (x - 1, y) not in self.moves_made:
+                self.nearby_moves.append((x - 1, y))
+            if (x + 1, y) not in self.moves_made:
+                self.nearby_moves.append((x + 1, y))
+            if (x, y - 1) not in self.moves_made:
+                self.nearby_moves.append((x, y - 1))
+            if (x, y + 1) not in self.moves_made:
+                self.nearby_moves.append((x, y + 1))
 
     def get_move(self):
         if len(self.nearby_moves) > 0:  # if we have tiles nearby where there was a hit
             x, y = self.nearby_moves.pop()
             is_hit = self.player_board.check_hit([x, y])
+            self.moves_made.append((x, y))  # mark that we made this move so we don't repeat it
             if is_hit is True:
                 self.hit_list.append((x, y))
             return x, y
 
         if len(self.hit_list) == 0:  # if there is nothing in the hit_list, choose at random
-            rand_col = random.randint(0, 9)
-            rand_row = random.randint(0, 9)
-            is_hit = self.player_board.check_hit([rand_row, rand_col])  # make move and check if hit
-            if is_hit is True:  # check if hit, if so add to hit_list
-                self.hit_list.append((rand_row, rand_col))
+            rand_row, rand_col = self.random_attack()
             return rand_row, rand_col
 
         else:  # there is something in the hit list
-            x, y = self.hit_list.pop()
-            if x == 0 or y == 0 or x == 9 or y == 9:  # "walls" of the board
-                if x == 0 and y == 0:  # top left corner
-                    self.nearby_moves.append((x, y + 1))
-                    self.nearby_moves.append((x + 1, y))
-                elif x == 9 and y == 9:  # bottom right corner
-                    self.nearby_moves.append((x - 1, y))
-                    self.nearby_moves.append((x, y - 1))
-                elif x == 0 and y == 9:  #
-                    self.nearby_moves.append((x + 1, y))
-                    self.nearby_moves.append((x, y - 1))
-                elif x == 9 and y == 0:
-                    self.nearby_moves.append((x - 1, y))
-                    self.nearby_moves.append((x, y + 1))
-                elif x == 0:
-                    self.nearby_moves.append((x + 1, y))
-                    self.nearby_moves.append((x, y + 1))
-                    self.nearby_moves.append((x, y - 1))
-                elif x == 9:
-                    self.nearby_moves.append((x - 1, y))
-                    self.nearby_moves.append((x, y - 1))
-                    self.nearby_moves.append((x, y + 1))
-                elif y == 0:
-                    self.nearby_moves.append((x - 1, y))
-                    self.nearby_moves.append((x + 1, y))
-                    self.nearby_moves.append((x, y + 1))
-                elif y == 9:
-                    self.nearby_moves.append((x + 1, y))
-                    self.nearby_moves.append((x - 1, y))
-                    self.nearby_moves.append((x, y - 1))
-            else:  # not on the walls or corners so we can move anywhere
-                self.nearby_moves.append((x - 1, y))
-                self.nearby_moves.append((x + 1, y))
-                self.nearby_moves.append((x, y - 1))
-                self.nearby_moves.append((x, y + 1))
-
+            self.find_nearby_moves()
             x, y = self.nearby_moves.pop()
             is_hit = self.player_board.check_hit([x, y])
             if is_hit is True:
